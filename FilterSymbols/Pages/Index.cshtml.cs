37c06c56dfba36e67e55baac54e506089b0735e0
Kevin Cottrill <KCottrill088@GMail.com>
2018-07-17 23:37:59 -0500

    Code cleanup

--- FilterSymbols/Pages/Index.cshtml.cs
+++ FilterSymbols/Pages/Index.cshtml.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using FilterSymbols.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
-using FilterSymbols.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
